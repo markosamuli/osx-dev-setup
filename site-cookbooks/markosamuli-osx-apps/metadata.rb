1f461768e81234f3cf32f2cca58677a845d86ff1
Marko Kirves <marko.kirves@thisisdare.com>
2013-06-11 14:56:28 +0100

    Installing homebrew packages does not work

--- site-cookbooks/markosamuli-osx-apps/metadata.rb
+++ site-cookbooks/markosamuli-osx-apps/metadata.rb
@@ -3,9 +3,10 @@ maintainer       'Marko Samuli Kirves'
 license          'MIT'
 description      'Recipes to install common OSX applications on Mountain Lion'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-supports         'mac_os_x'
-depends          'sprout-osx-base'
-depends          'sprout-osx-settings'
-depends          'sprout-osx-apps'
-depends          'dmg'
+version          '0.1.1'
+supports          "mac_os_x"
+depends           "dmg"
+depends           "osx"
+depends           "sprout-osx-base"
+depends           "sprout-osx-apps"
+depends           "sprout-osx-settings"
