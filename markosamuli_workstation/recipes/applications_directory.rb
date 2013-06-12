9f6288de411913193b6847b8db431c8c52c7c990
Marko Samuli Kirves <marko.kirves@thisisdare.com>
2013-06-12 19:30:49 +0100

    Remove /Applications directory permissions change recipe that breaks the system

--- markosamuli_workstation/recipes/applications_directory.rb
+++
@@ -1,6 +0,0 @@
-# We need to make sure the Applications directory is owned by the current user,
-# otherwise all application installations will fail
-directory "/Applications" do
-  owner node['current_user']
-  action :create
-end
