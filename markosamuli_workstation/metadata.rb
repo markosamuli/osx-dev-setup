662c5177e060ab23a16c9a47e53c000404d197e0
Marko Samuli Kirves <marko.kirves@thisisdare.com>
2013-06-12 19:31:55 +0100

    markosamuli_workstation v0.1.6

--- markosamuli_workstation/metadata.rb
+++ markosamuli_workstation/metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Marko Samuli Kirves'
 license          'MIT'
 description      'Install development workstation on Mountain Lion'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.1.6'
 supports          "mac_os_x"
 depends           "dmg"
 depends           "osx"
