7ca0f9960391f215765ebf81e14341d84f1bf8ce
Marko Samuli Kirves <marko.kirves@thisisdare.com>
2013-06-17 19:21:17 +0100

    PHP 5.4 recipe

---
+++ markosamuli_workstation/recipes/php54.rb
@@ -0,0 +1,33 @@
+include_recipe "sprout-osx-base::homebrew"
+
+execute "tap the josegonzalez repo" do
+    command "brew tap josegonzalez/php"
+    not_if { system("brew tap | grep 'josegonzalez' > /dev/null 2>&1") }
+end
+
+execute "tap the dupes repo" do
+    command "brew tap homebrew/dupes"
+    not_if { system("brew tap | grep 'dupes' > /dev/null 2>&1") }
+end
+
+brew "php54", {:brew_args => "--with-mysql --with-pgsql"}
+
+brew "php54-apc"
+brew "php54-memcached"
+brew "php54-inclued"
+brew "php54-http"
+brew "php54-xdebug"
+brew "php54-intl"
+brew "php54-yaml"
+brew "php54-imagick"
+brew "php54-mcrypt"
+
+template "/etc/apache2/other/php5.conf" do
+  source "php5.conf.erb"
+  owner "root"
+  action :create
+end
+
+execute "reload apache" do
+  command "sudo apachectl restart"
+end
